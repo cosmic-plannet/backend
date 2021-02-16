f7c79ca553cb5af010f6f8c99927986855b916be
kyueun <kelee0812.2@gmail.com>
2021-02-16 11:55:20 +0900

    add udt class

---
+++ UDT.py
@@ -0,0 +1,20 @@
+class achieve(object):
+    def __init__(self, type, created_at):
+        self.type = type
+        self.created_at = created_at
+
+
+class room(object):
+    def __init__(self, name, category, level, exp):
+        self.name = name
+        self.category = category
+        self.level = level
+        self.exp = exp
+
+
+class user(object):
+    def __init__(self, name, penalty=0, attendance=None, todo=None):
+        self.name = name
+        self.penalty = penalty
+        self.attendance = attendance
+        self.todo = todo

e8c162b6ec2b66b56696f4cce102b0667f62fd04
kyueun <kelee0812.2@gmail.com>
2021-02-16 13:03:52 +0900

    modify default value in user

--- UDT.py
+++ UDT.py
@@ -13,7 +13,7 @@ class room(object):
 
 
 class user(object):
-    def __init__(self, name, penalty=0, attendance=None, todo=None):
+    def __init__(self, name, penalty=0, attendance=[], todo={}):
         self.name = name
         self.penalty = penalty
         self.attendance = attendance

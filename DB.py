1d437fa288edbc9bd2a8dda0d2bf90fa97979b8b
kyueun <kelee0812.2@gmail.com>
2021-02-16 11:02:47 +0900

    add create user func

---
+++ DB.py
@@ -0,0 +1,27 @@
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement, dict_factory
+from datetime import datetime
+
+
+def create_user(email, name, interests):
+    cluster = Cluster(['127.0.0.1'])
+
+    session = cluster.connect('plannet')
+    session.row_factory = dict_factory
+    insert_user = session.prepare('INSERT INTO users (category, email, name, exp, interests, created_at) VALUES (?, ?, ?, 0, ?, ?)')
+
+    batch = BatchStatement()
+
+    created_at = datetime.now()
+
+    for category in interests:
+        batch.add(insert_user, (category, email, name, interests, created_at))
+
+    session.execute(batch)
+
+    result = session.execute('SELECT * FROM users WHERE category=\''+interests[0]+'\' and email=\''+email+'\'')[0]
+
+    cluster.shutdown()
+
+    return result

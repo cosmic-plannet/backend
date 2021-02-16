78ba41814d2118610089b65a2bb45e8be9d685b7
kyueun <kelee0812.2@gmail.com>
2021-02-16 22:35:51 +0900

    remove achive type

--- UDT.py
+++ UDT.py
@@ -1,9 +1,3 @@
-class achieve(object):
-    def __init__(self, type, created_at):
-        self.type = type
-        self.created_at = created_at
-
-
 class room(object):
     def __init__(self, name, category, level, exp):
         self.name = name

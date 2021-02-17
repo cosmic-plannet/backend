99f626b41bab3ce1dd45374569a198262ea6751b
kyueun <kelee0812.2@gmail.com>
2021-02-17 11:30:19 +0900

    modify user table schema

--- UDT.py
+++
@@ -1,6 +0,0 @@
-class room(object):
-    def __init__(self, name, category, level, exp):
-        self.name = name
-        self.category = category
-        self.level = level
-        self.exp = exp
